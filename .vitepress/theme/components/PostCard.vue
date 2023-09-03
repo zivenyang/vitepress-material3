6028017ee17c066bda954f6f64e95e29b7b4cfd7
杨子文 <yangziwen1994@gmail.com>
2023-09-03 19:35:28 +0800

    fix: add site.base to github page

--- .vitepress/theme/components/PostCard.vue
+++ .vitepress/theme/components/PostCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-card rounded="xl" v-ripple class="post-card" :href="post.url">
+    <v-card rounded="xl" v-ripple class="post-card" :href="site.base + post.url">
         <v-img cover height="200" class="rounded-xl" :src="post.cover"></v-img>
         <v-card-title>{{ post.title }}</v-card-title>
         <v-card-text>
@@ -9,6 +9,9 @@
 </template>
 <script setup lang="ts">
 import { Post } from '../types/common'
+import { useData } from 'vitepress';
+
+const { site, frontmatter } = useData();
 const props = defineProps<{ post: Post }>()
 </script>
 
@@ -16,5 +19,5 @@ const props = defineProps<{ post: Post }>()
 .post-card:hover {
     background-color: #c2e7ff;
     cursor: pointer;
-  }
+}
 </style>
